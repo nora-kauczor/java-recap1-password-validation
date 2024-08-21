58497dbb4faada49a6ba3712d6bf768dd8631c27
Nora Kauczor <n.kauczor@web.de>
2024-08-21 16:23:53 +0200

    write method to generate random password that matches all criteria

--- src/main/java/org/example/PasswordValidation.java
+++ src/main/java/org/example/PasswordValidation.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Random;
 
 
 public class PasswordValidation {
@@ -13,76 +14,84 @@ public class PasswordValidation {
         } else {
             System.out.println("Entered password does not match criteria.");
         }
-        }
-
-        public static boolean lengthIs8OrLonger (String password){
-            return password.length() > 7;
-        }
-
-        public static boolean containsDigit (String password){
-            boolean containsDigit = false;
-            char[] passwordCharacters = password.toCharArray();
-            for (char character : passwordCharacters) {
-                if (Character.isDigit(character)) {
-                    return containsDigit = true;
-                }
-            }
-            return containsDigit;
-        }
-
-        public static boolean containsUpperAndLowerCase (String password){
-            boolean containsUpperAndLowerCase = false;
-            boolean containsLowerCase = false;
-            boolean containsUpperCase = false;
-            char[] passwordCharacters = password.toCharArray();
-            for (char character : passwordCharacters) {
-                if (Character.isLowerCase(character)) {
-                    containsLowerCase = true;
-                }
-                ;
-            }
-            for (char character : passwordCharacters) {
-                if (Character.isUpperCase(character)) {
-                    containsUpperCase = true;
-                }
-            }
-            if (containsLowerCase == true && containsUpperCase == true) {
-                return true;
-            }
-            return containsUpperAndLowerCase;
-        }
-
-        public static boolean isNotWeak (String password){
-            String[] weakPasswords = {"password123", "123456", "qwerty", "abc123", "password1", "letmein", "welcome", "123456789", "admin", "Passwort", "iloveyou"};
-            if (Arrays.asList(weakPasswords).contains(password)) {
-                return false;
-            }
-            return true;
-        }
-
-        public static boolean containsSpecialCharacter (String password){
-            boolean containsSpecialCharacter = false;
-            char[] passwordCharacters = password.toCharArray();
-            for (char character : passwordCharacters) {
-                if (!Character.isDigit(character) && !Character.isLetter(character)) {
-                    return containsSpecialCharacter = true;
-                }
-            }
-            return containsSpecialCharacter;
-        }
-
-        public static boolean matchesAllCriteria (String password){
-            boolean matchesAllCriteria = false;
-            if (lengthIs8OrLonger(password) == true &&
-                    containsDigit(password) == true &&
-                    containsUpperAndLowerCase(password) == true &&
-                    isNotWeak(password) == true &&
-                    containsSpecialCharacter(password) == true) {
-                return true;
-            }
-            return matchesAllCriteria;
-        }
-
     }
 
-    ;
+    public static boolean lengthIs8OrLonger(String password) {
+        return password.length() > 7;
+    }
+
+    public static boolean containsDigit(String password) {
+        boolean containsDigit = false;
+        char[] passwordCharacters = password.toCharArray();
+        for (char character : passwordCharacters) {
+            if (Character.isDigit(character)) {
+                return containsDigit = true;
+            }
+        }
+        return containsDigit;
+    }
+
+    public static boolean containsUpperAndLowerCase(String password) {
+        boolean containsUpperAndLowerCase = false;
+        boolean containsLowerCase = false;
+        boolean containsUpperCase = false;
+        char[] passwordCharacters = password.toCharArray();
+        for (char character : passwordCharacters) {
+            if (Character.isLowerCase(character)) {
+                containsLowerCase = true;
+            }
+            ;
+        }
+        for (char character : passwordCharacters) {
+            if (Character.isUpperCase(character)) {
+                containsUpperCase = true;
+            }
+        }
+        if (containsLowerCase == true && containsUpperCase == true) {
+            return true;
+        }
+        return containsUpperAndLowerCase;
+    }
+
+    public static boolean isNotWeak(String password) {
+        String[] weakPasswords = {"password123", "123456", "qwerty", "abc123", "password1", "letmein", "welcome", "123456789", "admin", "Passwort", "iloveyou"};
+        if (Arrays.asList(weakPasswords).contains(password)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean containsSpecialCharacter(String password) {
+        boolean containsSpecialCharacter = false;
+        char[] passwordCharacters = password.toCharArray();
+        for (char character : passwordCharacters) {
+            if (!Character.isDigit(character) && !Character.isLetter(character)) {
+                return containsSpecialCharacter = true;
+            }
+        }
+        return containsSpecialCharacter;
+    }
+
+    public static boolean matchesAllCriteria(String password) {
+        boolean matchesAllCriteria = false;
+        if (lengthIs8OrLonger(password) &&
+                containsDigit(password) &&
+                containsUpperAndLowerCase(password) &&
+                isNotWeak(password)&&
+                containsSpecialCharacter(password)) {
+            return true;
+        }
+        return matchesAllCriteria;
+    }
+
+    public static String generatePassword() {
+        Random rand = new Random();
+        int numberOfChars = rand.nextInt(18);
+        char[] passwordArray = new char[numberOfChars];
+        for (int i = 0; i < passwordArray.length; i++) {
+            passwordArray[i] = (char) (random.nextInt(65536));
+        }
+        if (matchesAllCriteria(passwordArray.toString())){
+            return passwordArray.toString();
+        }
+    };
