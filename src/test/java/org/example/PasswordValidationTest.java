03a56ad704f8326155d319a2f2b654a2a67a0142
Nora Kauczor <n.kauczor@web.de>
2024-08-21 14:50:43 +0200

    add more tests for password length

--- src/test/java/org/example/PasswordValidationTest.java
+++ src/test/java/org/example/PasswordValidationTest.java
@@ -5,12 +5,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordValidationTest {
-@Test
-    void lengthIs8OrLonger_returnsFalse_whenCalledWith7CharString(){
-    String password = new String(new char[7]);
-    boolean expected = false;
-    boolean actual = PasswordValidation.lengthIs8OrLonger(password);
-            assertEquals(expected, actual);
+    @Test
+    void lengthIs8OrLonger_returnsFalse_whenCalledWith7CharString() {
+        String password = new String(new char[7]);
+        boolean expected = false;
+        boolean actual = PasswordValidation.lengthIs8OrLonger(password);
+        assertEquals(expected, actual);
+    }
 
-}
+    @Test
+    void lengthIs8OrLonger_returnsTrue_whenCalledWith25CharString() {
+        String password = new String(new char[25]);
+        boolean expected = true;
+        boolean actual = PasswordValidation.lengthIs8OrLonger(password);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void lengthIs8OrLonger_returnsTrue_whenCalledWith8CharString() {
+        String password = new String(new char[8]);
+        boolean expected = true;
+        boolean actual = PasswordValidation.lengthIs8OrLonger(password);
+        assertEquals(expected, actual);
+    }
 }
